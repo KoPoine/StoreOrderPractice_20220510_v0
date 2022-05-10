f42974b7644a496bab4d0bea75a925a8f737952c
nepp.plus <nepp.plus@gamil.com>
2022-05-10 17:14:24 +0900

    ListView 어댑터 뼈대 생성

---
+++ app/src/main/java/com/neppplus/storeorderpractice_20220510/adapters/StoreListViewAdapter.kt
@@ -0,0 +1,26 @@
+package com.neppplus.storeorderpractice_20220510.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.neppplus.storeorderpractice_20220510.models.StoreData
+
+class StoreListViewAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<StoreData>
+) : ArrayAdapter<StoreData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(resId, null)
+        }
+        val row = tempRow!!
+
+        return row
+    }
+
+}
