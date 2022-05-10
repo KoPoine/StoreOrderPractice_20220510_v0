a0513804a58d585cdff1f25b99cee09af723db73
nepp.plus <nepp.plus@gamil.com>
2022-05-10 18:11:48 +0900

    닉네임 변경 로직 완료

--- app/src/main/java/com/neppplus/storeorderpractice_20220510/EditNickActivity.kt
+++ app/src/main/java/com/neppplus/storeorderpractice_20220510/EditNickActivity.kt
@@ -1,7 +1,11 @@
 package com.neppplus.storeorderpractice_20220510
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_edit_nick.*
 
 class EditNickActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +15,19 @@ class EditNickActivity : AppCompatActivity() {
         setValues()
     }
 
-    fun setupEvents(){}
+    fun setupEvents(){
+        changeBtn.setOnClickListener {
+            val inputNick = inputNickEdt.text.toString()
+            if (inputNick.isBlank()) {
+                Toast.makeText(this, "올바른 닉네임을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val resultIntent = Intent()
+            resultIntent.putExtra("inputNick", inputNick)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
+    }
 
     fun setValues() {}
 }
--- app/src/main/java/com/neppplus/storeorderpractice_20220510/fragments/ProfileFragment.kt
+++ app/src/main/java/com/neppplus/storeorderpractice_20220510/fragments/ProfileFragment.kt
@@ -1,13 +1,20 @@
 package com.neppplus.storeorderpractice_20220510.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.neppplus.storeorderpractice_20220510.EditNickActivity
 import com.neppplus.storeorderpractice_20220510.R
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment :Fragment() {
+
+    val REQ_FOR_NICK = 1004
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,5 +25,20 @@ class ProfileFragment :Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        changeBtn.setOnClickListener {
+            val myIntent = Intent(requireContext(), EditNickActivity::class.java)
+            startActivityForResult(myIntent, REQ_FOR_NICK)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQ_FOR_NICK -> {
+                    nickTxt.text = data?.getStringExtra("inputNick")
+                }
+            }
+        }
     }
 }
