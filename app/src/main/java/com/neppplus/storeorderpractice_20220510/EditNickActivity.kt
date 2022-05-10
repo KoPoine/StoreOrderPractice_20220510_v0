62b7af9ad07277b74a05e2fd46579c26ac1c0417
nepp.plus <nepp.plus@gamil.com>
2022-05-10 18:03:12 +0900

    profile 변경 xml 작성

---
+++ app/src/main/java/com/neppplus/storeorderpractice_20220510/EditNickActivity.kt
@@ -0,0 +1,17 @@
+package com.neppplus.storeorderpractice_20220510
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNickActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nick)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){}
+
+    fun setValues() {}
+}
