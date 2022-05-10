18818ace24876db3ba77b808c39afba1e536d8ba
nepp.plus <nepp.plus@gamil.com>
2022-05-10 16:53:39 +0900

    MainViewPager 어댑터 뼈대 생성

--- app/src/main/java/com/neppplus/storeorderpractice_20220510/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/storeorderpractice_20220510/adapters/MainViewPagerAdapter.kt
@@ -1,4 +1,28 @@
 package com.neppplus.storeorderpractice_20220510.adapters
 
-class MainViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.neppplus.storeorderpractice_20220510.fragments.PizzaStoreFragment
+import com.neppplus.storeorderpractice_20220510.fragments.ProfileFragment
+
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+//    뷰페이저가 가지고 있을 프래그먼트의 숫자
+    override fun getCount() = 2
+
+//    각 페이지가 보여줄 프래그먼트
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> PizzaStoreFragment()
+            else -> ProfileFragment()
+        }
+    }
+
+//    TabLayout에 보여줄 페이지 제목
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+            0 -> "피자 주문"
+            else -> "내 정보 설정"
+        }
+    }
 }
