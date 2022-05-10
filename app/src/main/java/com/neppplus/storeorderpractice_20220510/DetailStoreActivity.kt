d57d05c2eb98d30bdf61e1fe848de0743b87b3d7
nepp.plus <nepp.plus@gamil.com>
2022-05-10 17:56:03 +0900

    권한 획득 처리, DetailStoreActivity에서 전화 연결 처리

--- app/src/main/java/com/neppplus/storeorderpractice_20220510/DetailStoreActivity.kt
+++ app/src/main/java/com/neppplus/storeorderpractice_20220510/DetailStoreActivity.kt
@@ -1,8 +1,14 @@
 package com.neppplus.storeorderpractice_20220510
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import com.neppplus.storeorderpractice_20220510.models.StoreData
 import kotlinx.android.synthetic.main.activity_detail_store.*
 
@@ -18,7 +24,26 @@ class DetailStoreActivity : AppCompatActivity() {
         setValues()
     }
 
-    fun setupEvents() {}
+    fun setupEvents() {
+        callBtn.setOnClickListener {
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    val myUri = Uri.parse("tel:${storeData.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(this@DetailStoreActivity, "권한이 없습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+        }
+    }
 
     fun setValues() {
         nameTxt.text = storeData.name
