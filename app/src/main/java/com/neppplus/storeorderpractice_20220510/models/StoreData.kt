404bd39bbca046230970adab4e7838a4df64d979
nepp.plus <nepp.plus@gamil.com>
2022-05-10 17:09:06 +0900

    fragment xml에 리스트뷰 생성 StoreData 생성

---
+++ app/src/main/java/com/neppplus/storeorderpractice_20220510/models/StoreData.kt
@@ -0,0 +1,9 @@
+package com.neppplus.storeorderpractice_20220510.models
+
+class StoreData(
+    val name : String,
+    val phoneNum : String,
+    val rating : Double,
+    val logoImg : String,
+) {
+}
